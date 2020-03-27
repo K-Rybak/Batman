dc10daaf24ba1a3b279f849e9447e471c7526195
K-Rybak <62746770+K-Rybak@users.noreply.github.com>
2020-03-27 14:32:25 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  let tabsItem = $('.tabs-item');
+  console.log(tabsItem);
+
+  tabsItem.on('click', function(event) {
+    event.preventDefault();
+    let activeContent = $(this).attr('href');
+    $('.visible').toggleClass('visible');
+    $(activeContent).toggleClass('visible');
+    $('.tabs-item-active').toggleClass('tabs-item-active');
+    $(this).toggleClass('tabs-item-active');
+  });
+});
